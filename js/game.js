d6cc252de509821e7daf53dbd8ae372e4f153ae1
Agung Sundoro <muhagung2001@gmail.com>
2024-07-20 16:06:14 +0700

    feat: add responsive view to mobile

--- js/game.js
+++ js/game.js
@@ -1,7 +1,3 @@
-/**
- * Created by Jerome Renaux (jerome.renaux@gmail.com) on 15-09-18.
- */
-
 var Game = {};
 
 Game.preload = function(){
@@ -11,17 +7,10 @@ Game.preload = function(){
 };
 
 Game.create = function(){
-    var txt = this.add.bitmapText(0, 0, 'font', 'SCORE:', 38).setOrigin(0);
-    this.scoreTxt = this.add.bitmapText(txt.x+txt.width+10, 0, 'font', '0', 38).setOrigin(0);
-    Game.setScore();
-
-    var txt = this.add.bitmapText(0, 600, 'font', 'VISITS:', 20).setOrigin(0,1);
-    this.visitsTxt = this.add.bitmapText(txt.x+txt.width+10, 600, 'font', '0', 20).setOrigin(0,1);
-    Game.setVisits();
-
-    var logo = this.add.image(400,300,'logo').setInteractive();
-    logo.on('pointerdown',function(){
+    var logo = this.add.image(window.innerWidth / 2, window.innerHeight / 2,'logo').setInteractive();
+    logo.on('pointerdown',function(pointer){
         Game.updateScore(10);
+
         Game.scene.tweens.add(
             {
                 targets: logo,
@@ -31,7 +20,13 @@ Game.create = function(){
                 yoyo: true
             }
         );
+
     });
+
+
+    var txt = this.add.bitmapText((window.innerWidth / 2) - 100, logo.height, 'font', 'SCORE:', 38).setOrigin(0);
+    this.scoreTxt = this.add.bitmapText(txt.x+txt.width+10, logo.height, 'font', '0', 38).setOrigin(0);
+    Game.setScore();
 };
 
 Game.setScore = function(){
@@ -41,20 +36,11 @@ Game.setScore = function(){
     Game.scene.scoreTxt.setText(Game.scene.score);
 };
 
-Game.setVisits = function(){
-    // Gets the value stored in localStorage, or 0 if nothing is found
-    // Don't fortget to parseInt(), all values are stored as strings in localStorage
-    Game.scene.visits = (parseInt(localStorage.getItem('visits')) || 0) + 1;
-    Game.scene.visitsTxt.setText(Game.scene.visits);
-    // Immediately increment the stored visits counter
-    localStorage.setItem('visits',Game.scene.visits);
-};
 
 // ### Alternate way to save, by bundling all data into a JSON file stored as a single localStorage entry ###
 Game.saveFile = function(){
     var file = {
         score: Game.scene.score,
-        visits: Game.scene.visits
     };
     localStorage.setItem('saveFile',JSON.stringify(file));
 };
@@ -62,7 +48,6 @@ Game.saveFile = function(){
 Game.loadFile = function(){
     var file = JSON.parse(localStorage.getItem('saveFile'));
     Game.scene.score = file.score;
-    Game.scene.visits = file.visits;
 };
 // ##################"
 
@@ -75,8 +60,8 @@ Game.updateScore = function(increment){
 
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
     parent: 'game',
     scene: [Game]
 };
